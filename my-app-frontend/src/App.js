4c014de14f2b6ca36131fd4975dde4bcb06a7b5e
Ilolo Izu <iloloizu97@gmail.com>
2022-02-14 11:25:22 -0600

    react router dom test

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -3,9 +3,54 @@ import './App.css';
 function App() {
   return (
     <div >
+      {/* header component */}
       <header>
-        <p className="black">Hello</p>
+        <p className="black">Oishii Me Up</p>
+
+        
+          <button>Login</button>
+          <button>Turn off the Lights</button>
+  
+
       </header>
+{/* main container component */}
+      <body>
+        <div className="main_container">
+
+        </div>
+{/* nav bar component */}
+        <div className="nav_bar"> 
+          NavBar
+            <li>
+              burger
+            </li>
+            <li>
+              pizza
+            </li>
+            <li>
+              ramen
+            </li>
+            <li>
+              pastry
+            </li>
+        </div>
+{/* collection of food cards */}
+    <div className="food_cards"> 
+    {/* food card  */}
+      <div>
+        Food Card 
+        Restaurant name
+        Restaurant price
+        Restaurant likes
+        Like / Dislike Feature
+      </div>
+    </div>
+
+
+      </body>
+
+
+
     </div>
   );
 }
